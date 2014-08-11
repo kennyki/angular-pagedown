41509843a697022271575061a1592084e00c8626
Kenny Ki <knyki.12@gmail.com>
2014-08-11 13:30:10 +0800

    - init

---
+++ angular-pagedown.js
@@ -0,0 +1,54 @@
+// adapted from http://stackoverflow.com/a/20957476/940030
+// TODO: create its own repo with pagedown dependencies (so that I can add it through bower)
+angular.module("ui.pagedown", [])
+.directive("pagedown", function ($compile, $timeout) {
+    var nextId = 0;
+    var converter = Markdown.getSanitizingConverter();
+
+    converter.hooks.chain("preBlockGamut", function (text, rbg) {
+        return text.replace(/^ {0,3}""" *\n((?:.*?\n)+?) {0,3}""" *$/gm, function (whole, inner) {
+            return "<blockquote>" + rbg(inner) + "</blockquote>\n";
+        });
+    });
+    
+    return {
+        restrict: "E",
+        scope: {
+            content: "=",
+            help: "&"
+        },
+        link: function (scope, element, attrs) {
+
+            var editorUniqueId;
+
+            if (attrs.id == null) {
+                editorUniqueId = nextId++;
+            } else {
+                editorUniqueId = attrs.id;
+            }
+
+            var newElement = $compile(
+                '<div>' +
+                     '<div class="wmd-panel">' +
+                            '<div id="wmd-button-bar-' + editorUniqueId + '"></div>' +
+                            '<textarea class="wmd-input" id="wmd-input-' + editorUniqueId + '" ng-model="content"></textarea>' +
+                     '</div>' +
+                     '<div id="wmd-preview-' + editorUniqueId + '" class="pagedown-preview wmd-panel wmd-preview"></div>' +
+                '</div>')(scope);
+
+            // html() doesn't work
+            element.append(newElement);
+
+            var help = angular.isFunction(scope.help) ? scope.help : function () {
+                // TODO: allow option for setting this
+                alert("There is no help available");
+            };
+
+            var editor = new Markdown.Editor(converter, "-" + editorUniqueId, {
+                handler: help
+            });
+
+            editor.run();
+        }
+    }
+});
